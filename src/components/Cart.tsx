4fe8bf0d409beb405a8d6d4848da326f32e00e87
stian96 <stiankru@hiof.no>
2023-12-12 13:59:39 +0100

    implemented what to be rendered in the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,7 +6,21 @@ const Cart = () => {
     const [cartItem, setCartItem] = useState<CartItem[]>([])
 
     return (
-        <div>This is the shopping cart!</div>
+        <div className="cart">
+            { cartItem.map((item) => (
+                <div key={item.product.id}>
+                    <h3 className="cart__product-name">
+                        { item.product.title }
+                    </h3>
+                    <p className="cart__product-quantity">
+                        Quantity: { item.quantity }
+                    </p>
+                    <button className="cart__remove-btn">
+                        Remove from cart
+                    </button>
+                </div>
+            ))}
+        </div>
     )
 }
 
