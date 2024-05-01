817c47bcf7e87993d3e94807296d41ca1b85c753
stian96 <stiankru@hiof.no>
2024-05-01 14:29:32 +0200

    implemented buttons to increase/decrease quantity.

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,17 +1,23 @@
 import { CartItem } from "@/domain/types/cartItem";
+import { useCart } from "@/context/CartProvider";
+import { FaMinus, FaPlus } from "react-icons/fa";
 import '@/styles/cart.scss'
 
 type CartItemProps = {
     item: CartItem
-    removeFromCart: (productId: number) => void
 }
 
-export const CartItems = ({ item, removeFromCart }: CartItemProps) => {
+export const CartItems = ({ item }: CartItemProps) => {
+    const { removeFromCart, increaseQuantity, decreaseQuantity } = useCart();
 
     return (
         <div className='cart__item-container' key={item.product.id}>
             <p className="cart__item-container__product-name">{item.product.title}</p> 
-            <p className="cart__item-container__product-quantity">{item.quantity}</p>
+            <div className="cart__item-container__quantity-control">
+                <button onClick={() => decreaseQuantity(item.product.id)}><FaMinus /></button>
+                <p className="cart__item-container__product-quantity">{item.quantity}</p>
+                <button onClick={() => increaseQuantity(item.product.id)}><FaPlus /></button>
+            </div>
             <p className="cart__item-container__product-price">{item.product.price}</p>
             <button 
                 className="cart__item-container__remove-btn" 
